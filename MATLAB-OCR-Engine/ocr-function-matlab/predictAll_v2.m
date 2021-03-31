4f993a1ec00c8828f5455baf0b80b7395b8521a2
ayseceyda <ayseceydaolmez@gmail.com>
2021-03-31 12:00:48 +0300

    adding v2 for limitless # of image reading

---
+++ MATLAB-OCR-Engine/ocr-function-matlab/predictAll_v2.m
@@ -0,0 +1,28 @@
+filePattern = fullfile('C:\Users\Analythinx\Documents\MATLAB\digital', '*.JPG');
+files = dir(filePattern);
+
+for i=1:length(files)
+    filename = files(i);
+    fname = filename.name;
+    predict(fname);
+end
+
+function predict(filename)
+
+    file = fullfile('output', filename);
+    res = imread(file);
+    
+    ocrRes = ocr(res,'CharacterSet', '0123456789', 'TextLayout', 'Line', 'Language', 'C:\Program Files\Tesseract-OCR\tessdata\original.traineddata');
+    
+    disp(filename);
+    disp(ocrRes.Text);
+    
+    % Save to txt document
+    f = fopen('C:\Users\Analythinx\Documents\MATLAB\results.txt', 'a+');
+    
+    for i= 1 :numel(ocrRes)
+        fprintf(f,'%s,',filename);
+        fprintf(f,'%s\r\n',ocrRes(i).Text);
+    end
+    %fclose("all");
+end
